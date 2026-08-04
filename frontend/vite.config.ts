431a45151ab53d6d0f13f75570ce53b5ce48ae3f
MdodoIng <mdodoyt2@gmail.com>
2026-08-04 18:33:21 +0530

    feat: update CORS configuration and adjust framework in Vercel settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
+  build: {
+    outDir: "dist",
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.API_URL,
-        changeOrigin: true,
-      },
-    },
-  },
-    ssr: {
-    noExternal: ["three-forcegraph", "react-force-graph-3d", "three-spritetext"]
-  }
 });
